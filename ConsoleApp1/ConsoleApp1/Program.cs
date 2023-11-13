a4931935d63dbd083dc7ef1c383a21e75f5947af
Gorbunov-Danila <gorbunovdanil345@gmail.com>
2023-11-13 12:36:15 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class HelloWorld
+{
+    public static long Counter = 0;
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("[Program] Введите число: ");
+        int N = int.Parse(Console.ReadLine());
+        var Timer = DateTime.Now;
+        Console.WriteLine("[Program] Результат: " + FibRecursive(N).ToString());
+        Console.WriteLine("[Program] Количество выполнений: " + Counter.ToString());
+        Console.WriteLine("[Program] Время выполнения: " + DateTime.Now.Subtract(Timer).TotalSeconds + " сек.");
+    }
+    static int FibRecursive(int Number)
+    {
+        Counter++;
+        if (Number <= 1) return Number;
+        else return FibRecursive(Number - 1) + FibRecursive(Number - 2);
+    }
+}
